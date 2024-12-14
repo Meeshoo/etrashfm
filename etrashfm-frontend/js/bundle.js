43f5366d30eca9c81836be2cc0dbe4935e1c2553
Meeshoo <mitch1080@hotmail.co.uk>
2024-12-14 13:20:56 +0000

    Improve player UI

--- etrashfm-frontend/js/bundle.js
+++ etrashfm-frontend/js/bundle.js
@@ -1,6 +1,6 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const YTPlayer = require('yt-player')
-const player = new YTPlayer('#player', {autoplay: true, modestBranding: true})
+const player = new YTPlayer('#player', {width: 1280, height: 720, autoplay: true, modestBranding: true})
 
 API_URL = "http://localhost:8000"
 
@@ -12,26 +12,6 @@ window.onload = function() {
 player.on('ended', () => {
   setTimeout(() => loadCurrentVideo(), 1000);
 })
-
-
-// async function seekToServerTime() {
-//   const url = API_URL.concat("/getcurrentvideotime");
-//   try {
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error(`Response status: ${response.status}`);
-//     }
-
-//     video_time = await response.json();
-
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-
-//   console.log("Time to seek to : ".concat(video_time))
-//   player.seek(video_time)
-
-// }
 
 async function loadCurrentVideo() {
   const url = API_URL.concat("/getcurrentvideoid");
--- etrashfm-frontend/js/index.js
+++ etrashfm-frontend/js/index.js
@@ -1,5 +1,5 @@
 const YTPlayer = require('yt-player')
-const player = new YTPlayer('#player', {autoplay: true, modestBranding: true})
+const player = new YTPlayer('#player', {width: 1280, height: 720, autoplay: true, modestBranding: true})
 
 API_URL = "http://localhost:8000"
 
@@ -11,26 +11,6 @@ window.onload = function() {
 player.on('ended', () => {
   setTimeout(() => loadCurrentVideo(), 1000);
 })
-
-
-// async function seekToServerTime() {
-//   const url = API_URL.concat("/getcurrentvideotime");
-//   try {
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error(`Response status: ${response.status}`);
-//     }
-
-//     video_time = await response.json();
-
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-
-//   console.log("Time to seek to : ".concat(video_time))
-//   player.seek(video_time)
-
-// }
 
 async function loadCurrentVideo() {
   const url = API_URL.concat("/getcurrentvideoid");
